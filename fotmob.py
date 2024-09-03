d809b8e8b214123159e84a8f54bbe138214d03b0
bariscanyeksin <82212277+bariscanyeksin@users.noreply.github.com>
2024-09-03 13:21:41 +0300

    Update fotmob.py

--- fotmob.py
+++ fotmob.py
@@ -921,22 +921,22 @@ if int(player1_id) > 0 and int(player2_id) > 0:
                                 padding-right: 10px;
                             }
                         }
-                        .row-widget.stDownloadButton {
+                        .stDownloadButton {
                             display: flex;
                             justify-content: center;
                             text-align: center;
                         }
-                        .row-widget.stDownloadButton button {
+                        .stDownloadButton button {
                             background-color: rgba(51, 51, 51, 0.17);
                             color: gray;  /* Text color */
                             border: 0.5px solid gray;  /* Thin gray border */
                             transition: background-color 0.5s ease;
                         }
-                        .row-widget.stDownloadButton button:hover {
+                        .stDownloadButton button:hover {
                             background-color: rgba(51, 51, 51, 0.65);
                             border: 1px solid gray;  /* Thin gray border */
                         }
-                        .row-widget.stDownloadButton button:active {
+                        .stDownloadButton button:active {
                             background-color: rgba(51, 51, 51, 0.17);
                             color: gray;  /* Text color */
                             border: 0.5px solid gray;  /* Thin gray border */
